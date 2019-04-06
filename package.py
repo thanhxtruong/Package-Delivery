c90a0a2ea40bd691c0cbf043e62b1c485667a513
Thanh Truong <thanhgvsu@gmail.com>
2019-04-06 07:13:18 -0400

    Create Package class
    Created constructor for Package class

---
+++ package.py
@@ -0,0 +1,9 @@
+class Package:
+
+    # Constructor for a new Package object.
+    def __init__(self, pkg_id, address, city, zipCode, weight):
+        self.pkg_id = pkg_id
+        self.address = address
+        self.city = city
+        self.zipCode = zipCode
+        self.weight = weight
